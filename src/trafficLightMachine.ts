f786f2866df46cc5377fe949707bc238addc7403
rosarie <rosariosanchezsampietro@gmail.com>
2022-05-12 20:53:46 -0300

    First commit

---
+++ src/trafficLightMachine.ts
@@ -0,0 +1,55 @@
+import { createMachine } from "xstate";
+
+
+type TrafficLightEvents = 
+|{ type : "NEXT" } 
+| { type : "TURN_ON" }
+| { type: "TURN_OFF"};
+
+type TrafficLightStates =
+    |{   value: {ON: "red"}; context: undefined  }
+    |{   value: {ON: "yellow"}; context: undefined   }
+    |{   value: {ON: "green"}; context: undefined    }
+    |{   value: "OFF"; context: undefined };
+
+
+export const trafficLightMachine = createMachine<
+    undefined,
+    TrafficLightEvents,
+    TrafficLightStates
+>({
+    initial: "OFF",
+    id: "traffic-light",
+    states: {
+        ON: {
+            initial: "red",
+            on: { TURN_OFF: "OFF" },
+            states: {
+                green: {
+                    on: { NEXT: "yellow" },
+                    after: {
+                        3500: "yellow",
+                    }
+                    },
+                yellow: {
+                    on: { NEXT: "red" },
+                    after :{
+                        1500: "red",
+                    }
+                    },
+                red: {
+                    on: { NEXT: "green" },
+                    after: {
+                        5000: "green",
+                    }
+                    },
+            }
+        },
+        OFF: {
+            on: {
+                TURN_ON : "ON"
+            },
+        },
+        
+    },
+});
